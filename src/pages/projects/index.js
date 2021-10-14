31dca720499ef1d06999c53637a3f22a2b5d6fd7
knirt <hayaku.ne@protonmail.com>
2021-10-14 10:29:12 -0700

    updating styling on projects page

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -11,16 +11,16 @@ import portfolioImg from "../../../static/portfolio.png"
 const projectInfo = {
     library: {
         name: "Book Collection",
-        intro: "This is a web app I built to store and display my collection of literature and academic writing",
+        intro: "This is a web app I built to store and display my collection of literature and academic writing:",
         description:
-            "I used react, typescript, and redux to build the app. The collection can be searched and filtered by catagory or author/title, and there is a favorites page that acts like a shopping cart. Note: It is very slow in initial load because I have not set it up as a subdomain on my VPS yet. That is coming soon. Also, I am currently working on a backend that allows account creation and user authentication.",
+            "I used react, typescript, and redux to build the app. The collection can be searched and filtered by catagory or author/title, and there is a favorites page that acts like a shopping cart. Note: I have not set it up as a subdomain on my VPS yet, I plan on doing this soon and adding a database with a user authentication backend.",
         imgRef: libraryImg,
         codeLink: "https://github.com/knirt/library-website",
         siteLink: "https://library-collection-797473.netlify.app",
     },
     recipes: {
         name: "Forkify",
-        intro: "My first ever web app, a recipe site. This was a project I did as part of a Udemy course",
+        intro: "My first ever web app, a recipe site. This was a project I did as part of a Udemy course:",
         description:
             "Created using nothing but vanilla JS. Its a basic recipe site which calls an API (not developed by me) with bookmarking, keyword search, and the ability to submit new recipies of your own via POST request. Bookmarks are saved via local browser storage, I am not planning on changing this but I am planning on adding user accounts with a backend on the library site.",
         imgRef: recipeImg,
@@ -35,9 +35,9 @@ const projectInfo = {
     },
     portfolio: {
         name: "Portfolio Site",
-        intro: "This site: my portfolio site",
+        intro: "This site: my portfolio site:",
         description:
-            "This site was made using gatsby.js, a static site generator that utilizes react to produce static html sites efficiently. It is able to harness the efficiency of react without the bloat. It was styled using SASS.",
+            "This site was made using gatsby.js, a static site generator that utilizes react to produce static html sites efficiently. It is able to harness the efficiency of react without the bloat. It was styled using SASS, a styling language similar to CSS but far more efficient due to the addition of functions and a generally more intuitive syntax. It is precompiled into normal CSS.",
         imgRef: portfolioImg,
         codeLink: "https://github.com/knirt/portfolio-site",
         siteLink: "https://mtrinkus.xyz",
